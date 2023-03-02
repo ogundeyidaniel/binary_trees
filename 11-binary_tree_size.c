8deaa0cfb0649a133bd1569f2b519bef153f9df3
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-01 20:17:05 -0800

    binary

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,7 +8,7 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t h_left, h_right;
-	
+
 	if (tree == NULL)
 		return (0);
 	h_left = binary_tree_size(tree->left);
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,20 +1,31 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_leaves - Counts the leaves in a binary tree.
- * @tree: A pointer to the root node of the tree to count the leaves of.
- *
- * Return: The number of leaves in the tree.
+ * binary_tree_is_leaf - check node leaf or not
+ * @node: node to check
+ * Return: 1 if node if leaf 0 if not
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+/**
+ * binary_tree_leaves - measures the leaves of the tree
+ * @tree:pointer to the root node of the tree
+ * Return: 0 if tree is NULL else the size leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	size_t h_left, h_right;
 
-	if (tree)
-	{
-		leaves += (!tree->left && !tree->right) ? 1 : 0;
-		leaves += binary_tree_leaves(tree->left);
-		leaves += binary_tree_leaves(tree->right);
-	}
-	return (leaves);
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	h_left = binary_tree_leaves(tree->left);
+	h_right = binary_tree_leaves(tree->right);
+	return (h_right + h_left);
 }
