f28dba40d80bf73efeb18ffa88d6688b73933ce2
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-01 18:15:10 -0800

    header file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,25 +1,22 @@
 #include "binary_trees.h"
 
-/**
- * binary_tree_node - Creates a binary tree node.
- * @parent: A pointer to the parent of the node to create.
- * @value: The value to put in the new node.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+#include <stddef.h>
+/*
+ * binary_tree_node - Create a binary tree node
+ * @parent: root node of the tree
+ * @value: the value in the root note
+ * Return: binary tree or NULL on fail
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
-
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
+	binary_tree_t  *tree;
+	
+	tree = malloc(sizeof(binary_tree_t));
+	if (tree == NULL)
 		return (NULL);
-
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-
-	return (new);
+	tree->parent = parent;
+	tree->n = value;
+	tree->right = NULL;
+	tree->left = NULL;
+	return (tree);
 }
