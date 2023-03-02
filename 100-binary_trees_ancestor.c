c4cb8c0cde8c2784894d752fb9e80898ad733a69
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-01 21:44:03 -0800

    Binary

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,7 +5,8 @@
  * @second:node
  * Return: pointer or NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
 	binary_tree_t *node;
 
