1d036a511bbbe2c24161f79abcab06a9bcc8091e
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-01 23:08:44 -0800

    binary_tree

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include "limits.h"
 #include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
@@ -49,4 +50,5 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
