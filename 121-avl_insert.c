e617c11b0aca3b7ba8c3616a5f170a6622d72294
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-02 14:41:13 -0800

    121-avl_insert.c

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -9,54 +9,47 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *tmp = *tree;
-
-	bst_t *second = NULL;
-
-	bst_t *new = binary_tree_node(NULL, value);
-
-	if (*tree == NULL)
-		*tree = new;
-
-	while (tmp)
-	{
-		second = tmp;
-
-		if (value < tmp->n)
-			tmp = tmp->left;
-		else if (value > tmp->n)
-			tmp = tmp->right;
-		else if (value == tmp->n)
-			return (NULL);
-	}
-
-	if (second == NULL)
-		second = new;
-	else if (value < second->n)
-	{
-		second->left = new;
-		new->parent = second;
-	}
-	else
-	{
-		second->right = new;
-		new->parent = second;
-	}
-	return (new);
+		bst_t *tmp = *tree;
+		bst_t *second = NULL;
+		bst_t *new = binary_tree_node(NULL, value);
+		if (*tree == NULL)
+			*tree = new;
+		while (tmp)
+		{
+			second = tmp;
+			if (value < tmp->n)
+				tmp = tmp->left;
+			else if (value > tmp->n)
+				tmp = tmp->right;
+			else if (value == tmp->n)
+				return (NULL);
+		}
+		if (second == NULL)
+			second = new;
+		else if (value < second->n)
+		{
+			second->left = new;
+			new->parent = second;
+		}
+		else
+		{
+			second->right = new;
+			new->parent = second;
+		}
+		return (new);
 }
 
 /**
  * avl_insert - inserts a value in an AVL Tree
  * @tree: a double pointer to the root node of the AVL tree
  * @value: value to insert
- *
+ * 
  * Return: a pointer to the created node
  * NULL on failure
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
 	int balance = 0;
-
 	avl_t *node = bst_insert(tree, value);
 
 	balance = binary_tree_balance(*tree);
@@ -75,6 +68,5 @@ avl_t *avl_insert(avl_t **tree, int value)
 		node->right = binary_tree_rotate_right(node->right);
 		return (binary_tree_rotate_left(node));
 	}
-
 	return (node);
 }
